df698a1801fcdfeb261e2f893e6967051c55e103
Pearceton Milton <pgmilton05@gmail.com>
2026-01-22 15:17:50 -0500

    Removed testing code and added a few comments.

--- PokemonColosseum.py
+++ PokemonColosseum.py
@@ -2,9 +2,11 @@ import random
 from functions.ParseCSV import ParsePokemonData, ParsePokemonMoves
 from Pokemon import AssignPokemon, AssignMoves, Damage
 
+# Get player name and welcome message
 print("Welcome to Pokemon Colosseum!\n")
 player_name = input("What is your name?: ")
 
+# Assign Pokemon to each team
 team_rocket_pokemon = AssignPokemon()
 tr_pokemon_1 = team_rocket_pokemon[0]
 tr_pokemon_2 = team_rocket_pokemon[1]
@@ -15,6 +17,7 @@ player_pokemon_1 = player_pokemon[0]
 player_pokemon_2 = player_pokemon[1]
 player_pokemon_3 = player_pokemon[2]
 
+# Display teams
 print("\nTeam Rocket enters! They come with", tr_pokemon_1 + ",", tr_pokemon_2 + ", and", tr_pokemon_3 + "!\n")
 print("Team", player_name + " enters! They come with", player_pokemon_1 + ",", player_pokemon_2 + ", and", player_pokemon_3 + "!\n")
 
@@ -25,18 +28,6 @@ coin_flip = random.choice(["Team Rocket", player_name])
 print(coin_flip, "will go first!\n")
 
 
-"""
-moves = (AssignMoves(tr_pokemon_1))
-# Get all move details
-moves_data = ParsePokemonMoves()
-
-
-# Test: Print assigned moves and their details
-for move_name in moves:
-    details = moves_data[move_name]
-    print(f"{move_name}: Type={details['Type']}, Power={details['Power']}, Accuracy={details['Accuracy']}")
-"""
-
 # Add Pokemon from each team to a queue
 tr_pokemon_queue = [tr_pokemon_1, tr_pokemon_2, tr_pokemon_3]
 player_pokemon_queue = [player_pokemon_1, player_pokemon_2, player_pokemon_3]
@@ -45,6 +36,7 @@ player_pokemon_queue = [player_pokemon_1, player_pokemon_2, player_pokemon_3]
 is_player_turn = (coin_flip == player_name)
 current_turn = "Player" if is_player_turn else "Team Rocket"
 
+# Function to switch turns
 def switch_turn(is_player_turn):
     return not is_player_turn
 
